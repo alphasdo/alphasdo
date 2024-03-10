a0fb4c947cdbb067e3132e5fb2bd9320756c974d
alphasdo <2568416067@qq.com>
2024-03-10 20:17:07 +0800

    3.10 类型转换与反射

--- test2.java
+++ test2.java
@@ -1,5 +1,17 @@
-public class test2 {
-    public static void main(String[] args) {{
-        System.out.println("1234");
+import java.lang.reflect.Field;
+public class test2 {   
+    private static int result = 42;  
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        
+        // 通过字符串名字获取字段对象
+        Field field = test2.class.getDeclaredField("result");
+        
+        // 设置字段为可访问
+        field.setAccessible(true);
+        
+        // 获取字段的值并输出
+        int value = (int) field.get(null);  // 如果是静态变量，第一个参数为null
+        System.out.println("value of result: " + value);
     }
 }
+
